b6a09783f1d47b975e33f7c22a2d04f5ca48316c
jatin <jatinramchandani15@gmail.com>
2024-04-04 18:40:55 +0530

    added api for fetching managers list

---
+++ Backend/Controller/manager.js
@@ -0,0 +1,47 @@
+require("dotenv");
+var axios = require("axios").default;
+
+const generateToken = async () => {
+  try {
+    var options = {
+      method: "POST",
+      url: "https://dev-34crl0ebsqxu7bk8.us.auth0.com/oauth/token",
+      headers: { "content-type": "application/x-www-form-urlencoded" },
+      data: new URLSearchParams({
+        grant_type: "client_credentials",
+        client_id: process.env.APP_AUTH0_CLIENT_ID,
+        client_secret: process.env.APP_AUTH0_CLIENT_SECRET,
+        audience: process.env.APP_AUTH0_AUDIENCE,
+      }),
+    };
+
+    const response = await axios.request(options);
+    const { data } = response;
+    return data.access_token;
+  } catch (error) {
+    res.status(500).json({ message: "some error occurred" });
+  }
+};
+
+const fetchManagers = async (req, res) => {
+  try {
+    const { role_id } = req.query;
+    const token = await generateToken();
+    let config = {
+      method: "get",
+      maxBodyLength: Infinity,
+      url: `https://dev-34crl0ebsqxu7bk8.us.auth0.com/api/v2/roles/${role_id}/users`,
+      headers: {
+        Accept: "application/json",
+        Authorization: `Bearer ${token}`,
+      },
+    };
+    const response = await axios.request(config);
+    const { data } = response;
+    res.status(200).json(data);
+  } catch (error) {
+    res.status(500).json({ message: "some error occurred" });
+  }
+};
+
+module.exports = { fetchManagers };
